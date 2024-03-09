79e98550b519cb05ccb17518762d9ef85f257a07
Kyungin Na <nakyungin93@gmail.com>
2024-03-09 19:53:02 +0100

    feat: add /heroes route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessageComponent } from './message/message.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [HeroesComponent, MessageComponent],
+  imports: [HeroesComponent, MessageComponent, RouterOutlet, RouterLink],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
